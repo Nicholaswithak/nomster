c5f14c08852700019a67019c5f1a360d7eeb38ca
nicholas <ncl1984@gmail.com>
2019-07-16 20:47:22 +0000

    lesson 28

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,10 +13,10 @@ class PlacesController < ApplicationController
   def create
     @place = current_user.places.create(place_params)
     if @place.valid?
-    redirect_to root_path
-  else
-    render :new, status: :unprocessable_entity
-  end
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
