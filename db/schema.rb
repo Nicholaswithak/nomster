13a9f62e6df03ee7573ff8d653e0a4648f4ec923
nicholas <ncl1984@gmail.com>
2019-07-19 00:35:12 +0000

    added rating section to nomster website

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2019_07_18_055639) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
@@ -42,6 +44,11 @@ ActiveRecord::Schema.define(version: 2019_07_18_055639) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
